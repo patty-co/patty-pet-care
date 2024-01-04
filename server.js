fde0e57adbc9ffac2b9a6222e7036d8b87f81624
Patricia Costa <pattylife13@gmail.com>
2024-01-04 15:45:16 +0000

    Configure Express to serve React app

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const path = require('path');
 
+
+//Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+//Routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
