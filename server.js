09220a5fd0d3b00d00c06ff6ebb4032b45d5912f
Patricia Costa <pattylife13@gmail.com>
2024-01-04 16:05:40 +0000

    Configure PostgreSQL with Sequelize

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+});
+
+//Test the database connection
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection successful');
+    } catch (error) {
+        console.error('Unable to connect to the database: ', error);
+    }
+})();
 
 
 //Middleware
