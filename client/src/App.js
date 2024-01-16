8afa6d6e03a4d56d16c365d7acf79c220cf15e25
Patricia Costa <pattylife13@gmail.com>
2024-01-16 02:21:34 +0000

    under construction page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
+import logo from './logo_img.png';
 import React from 'react';
 import './App.css';
+import './UnderConstruction.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <div className="bg-soft-green p-8">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="text-3xl font-bold text-coral">Patty's Pet Care</h1>
         </div>
-      </header>
+      </header> */}
+      <div>
+        <div className="header">
+          <img src={logo} alt="Patty's Pet Care Logo" className="logo" />
+        </div>
+        <div className="container">
+          <h1 className="under-construction">Em construção</h1>
+          <p>Novidades brevemente</p>
+        </div>
+      </div>
     </div>
   );
 }
